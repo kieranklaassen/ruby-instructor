46b24bcbd3066e64b9a1a47d746a9dd98f7ff551
Kieran Klaassen <kieranklaassen@gmail.com>
2024-02-01 11:03:23 -0600

    Initial

---
+++ lib/ruby/instructor.rb
@@ -0,0 +1,8 @@
+require "ruby/instructor/version"
+
+module Ruby
+  module Instructor
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ruby/instructor/version.rb
@@ -0,0 +1,5 @@
+module Ruby
+  module Instructor
+    VERSION = "0.1.0"
+  end
+end
